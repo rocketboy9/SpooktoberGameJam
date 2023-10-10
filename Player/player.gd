c6bfceec2f69bfcfe6c63e793a5e8a1f0ed5d343
rocketboy9 <diettrichdrew@gmail.com>
2023-10-10 19:38:21 -0400

    second commit with flashlight

---
+++ Player/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+var speed = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+	position += velocity *delta
+	move_and_slide()
+	
