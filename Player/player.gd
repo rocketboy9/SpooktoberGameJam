5328e01a245d2fdc48f9f4d2eb79cd3b65b8768e
Poseidon <ballycroyboy@gmail.com>
2023-10-10 20:02:47 -0400

    Added rudimentary sprint function
    Added rudimentary sprint function.

--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var speed = 100
+@export var speed = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +11,10 @@ func _ready():
 func _process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
-	position += velocity *delta
+	position += velocity * delta
+	if Input.is_action_just_pressed("sprint"):
+		speed *= 2
+	if Input.is_action_just_released("sprint"):
+		speed /= 2
 	move_and_slide()
 	
