cbf8c92a7441dba87257c8f2f6fc6427d37b9eaa
Poseidon <ballycroyboy@gmail.com>
2023-10-13 21:43:05 -0400

    Added code to move the ghost towards the player

--- Ghost/ghost.gd
+++ Ghost/ghost.gd
@@ -2,6 +2,17 @@ extends CharacterBody2D
 
 signal game_over()
 
+var player
+@export var speed = 100
+
+func _ready():
+	player = get_tree().get_first_node_in_group("Player")
+
+func _process(delta):
+	var normal_to_player = (player.position - position).normalized()
+	var velocity = normal_to_player * speed
+	position += velocity * delta
+
 func _on_death_area_body_entered(body):
 	if "lastDirectionLooked" in body:
 		game_over.emit()
